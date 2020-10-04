814fb87acb62ebbe956de76fd987a7944d8e1af5
herrysis404 <67797468+herrysis404@users.noreply.github.com>
2020-10-04 21:43:17 +0800

    Add files via upload

---
+++ tobrot/config.py
@@ -0,0 +1,34 @@
+from tobrot.sample_config import Config
+#Fill your all data, read readme for reference
+
+class Config(Config):
+#	TG_BOT_TOKEN= "1055727875:AAEE7Ms0YrktOD1nSffr5lP1hN7j93wbciA" #botgblk
+#	APP_ID = 1431038
+#	API_HASH = "a5db4ec0dc64a6a2a5f77e83620eacdf"
+#	OWNER_ID = "416377541" #ID of bot owner
+	TG_BOT_TOKEN= "1255639276:AAHFcNS-rLRox3hAHhT5eprESwIRWZ-OIQA" #aoitorbot
+#	TG_BOT_TOKEN= "1386265569:AAGleTt45di7OttolmWX0cAdhBiPAXhFkk4" #cloningbotid
+	APP_ID = 1649008
+	API_HASH = "933aae7abdc0b82fffccf3d69d1e7a91"
+	OWNER_ID = "1282345589" #ID of bot owner
+#	AUTH_CHANNEL = [-1001495399151]
+	DESTINATION_FOLDER = "newbotwithclone" #Name of your folder read readme
+	RCLONE_CONFIG = """type = drive\nclient_id = 1087960453228-1aisiq3pk3u329vmh3rcobnsundcjtee.apps.googleusercontent.com\nclient_secret = sl2iY065FYolOuv38sQ69ln_\nscope = drive\nroot_folder_id = 0AMbxjDSwbyTdUk9PVA\ntoken = {"access_token":"ya29.a0AfH6SMDoX8uS0-4Nupvwe-Q0TsvSTNSZhyf2x-_vD9UuWPr0RyB51qanlkudJozIpIUVv0g4zR1NpmBQrQaOW2KppWCe8OpEOtJHUZGZjUzsINU4QkcSY_3ZTuQn-pzMP4av7UGyJkKOEx-utGi8qTiZfZIDR98sjLw","token_type":"Bearer","refresh_token":"1//0gaAnnW2r-yJLCgYIARAAGBASNwF-L9IryrUE0lLip7ievnLxfL4-jslPc3CgAcvIbPG0eLiTRgln2nu_Z-y19RqyIpD7hQflS7s","expiry":"2020-07-06T01:58:03.627739532+08:00"}"""
+	#fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable
+	#ARIA_TWO_STARTED_PORT = 6801
+	#UPLOAD_AS_DOC = False
+	#mEDIT_SLEEP_TIME_OUT = 15
+	#AX_TIME_TO_WAIT_FOR_TORRENTS_TO_START = 600
+	FINISHED_PROGRESS_STR = "⬛"
+	UN_FINISHED_PROGRESS_STR = "⬜"
+	#LEECH_COMMAND = "leech@workeronebot"
+	#GLEECH_COMMAND = "gleech@workeronebot"
+	#TELEGRAM_LEECH_COMMAND_G = "tleech@workeronebot"
+	#YTDL_COMMAND = "ytdl@workeronebot"
+	#PYTDL_COMMAND_G = "pytdl@workeronebot"
+	#CANCEL_COMMAND_G = "cancel@workeronebot"
+	#GET_SIZE_G = "getsize@workeronebot"
+	#STATUS_COMMAND = "status@workeronebot"
+	#CLONE_COMMAND_G = "gclone@workeronebot"
+	#CUSTOM_FILE_NAME = "QU_"
+	INDEX_LINK = "https://files.nuub.workers.dev/1:"
