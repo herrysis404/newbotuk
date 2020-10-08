5f6340b2a6a57521a21a31088e258895d69f4fba
herrysis404 <67797468+herrysis404@users.noreply.github.com>
2020-10-08 11:41:13 +0800

    Update upload_to_tg.py

--- tobrot/helper_funcs/upload_to_tg.py
+++ tobrot/helper_funcs/upload_to_tg.py
@@ -176,7 +176,7 @@ async def upload_to_gdrive(file_upload, message, messa_ge, g_id):
             indexurl = f"{INDEX_LINK}/{file_upload}"
             tam_link = requests.utils.requote_uri(indexurl)
             LOGGER.info(tam_link)
-            button.append([pyrogram.InlineKeyboardButton(text="☁️   Visit   ☁️", url=f"{tam_link}"?a=view)])
+            button.append([pyrogram.InlineKeyboardButton(text="☁️   Visit   ☁️", url=f"{tam_link}?a=view")])
         button_markup = pyrogram.InlineKeyboardMarkup(button)
         await asyncio.sleep(EDIT_SLEEP_TIME_OUT)
         await messa_ge.reply_text(f"{file_upload}<a href='tg://user?id={g_id}'>🤒</a>\n📀 Size: {gjay}", reply_markup=button_markup)
